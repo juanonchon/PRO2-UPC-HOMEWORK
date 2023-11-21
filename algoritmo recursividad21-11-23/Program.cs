7ef9024c87d134782167897382f08a8b1e975f26
juanonchon <132412187+juanonchon@users.noreply.github.com>
2023-11-21 18:12:45 -0400

    exposicion

---
+++ algoritmo recursividad21-11-23/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace recursividad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(factorial(5));
+            Console.ReadLine();
+
+        }
+        public static int factorial(int n)
+        {
+            if (n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return n * factorial(n - 1);
+            }
+            
+        }
+
+        
+    }
+}
