a9bfdd2f653e45acc8fa34dca29d7eee482308dd
juanonchon <132412187+juanonchon@users.noreply.github.com>
2023-11-23 16:08:50 -0400

    tarea 23-11-23

---
+++ DominoRecusivo23-11-23/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Programacion2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+           DominoRecursiv0(A,B);
+        }   
+        static int A = 0;
+        static int B = 7;
+        public static void DominoRecursiv0(int A, int B){
+            if(A == 0){
+
+            }
+            if(B == 0)
+            
+            {
+                return;
+            }else{
+                int serieA = A + 1;
+                Console.WriteLine("SerieA: " + serieA);
+                A = serieA;  
+
+                int serieB = B - 1;
+                Console.WriteLine("SerieB: " + serieB);
+                B = serieB;  
+                DominoRecursiv0(A, B);   
+            }
+            
+        }
+    }
+
+}
